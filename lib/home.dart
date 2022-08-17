90583115d643d6ab2652592bc5f9b95e8c4ba9eb
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-08-17 20:15:23 -0300

    Setting background color as green and title of the app

--- lib/home.dart
+++ lib/home.dart
@@ -8,11 +8,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final _frases = [
+    "Não deixe para commitar amanhã o que se pode commitar hoje",
+    "Muito ajuda quem não atrapalha!",
+    "É só uma fase ruim, tudo irá piorar :)",
+    "Lute como nunca, perca como sempre"
+  ];
+
+  var _fraseGerada = "Clique abaixo para gerar uma frase";
+
+  void _gerarFrase() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text("Frases do dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        
+      ),
     );
   }
 }
