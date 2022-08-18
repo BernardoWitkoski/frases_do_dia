eb7bb1d95c6234c62f98124b546e99ab179de0c5
Bernardo Witkoski <bernardowitkoski@hotmail.com>
2022-08-18 20:33:34 -0300

    creating author table

---
+++ lib/database/app_database.dart
@@ -0,0 +1,13 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+void createDataBase() {
+  getDatabasesPath().then((dbPath) {
+    final String path = join(dbPath, 'dailyphrase.db');
+    openDatabase(path, onCreate: (db, version){
+      db.execute('CREATE TABLE author('
+          'id INTEGER PRIMARY KEY,'
+          'name TEXT');
+    }, version: 1);
+  });
+}
